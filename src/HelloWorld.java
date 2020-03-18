8df0a10963467c15e6db84185e8b22e118930377
ATIV <ATIV@DESKTOP-OFSQM0I>
2020-03-18 21:27:47 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("1. Sensuous");
+		System.out.println("2. Sensitive");
+		System.out.println("3. Cheerful");
+		System.out.println("4. Calm");
+		System.out.println("5. Exit");
+	}
+
+}
