4978b668fc6481d88ed2b92e79be8ae476206a9a
ATIV <ATIV@DESKTOP-OFSQM0I>
2020-03-20 21:16:09 +0900

    recreate my system

---
+++ src/EdmManagementSystem.java
@@ -0,0 +1,13 @@
+public class EdmManagementSystem {
+
+	public static void main(String[] args) {
+		System.out.println("---------Edm Management System Menu--------");
+		System.out.println(" 1. Add Music");
+		System.out.println(" 2. Delete Music");
+		System.out.println(" 3. Edit Music");
+		System.out.println(" 4. View Music");
+		System.out.println(" 5. Exit");
+		System.out.println(" Select one number between 1 ~ 5 ");
+	}
+
+}
